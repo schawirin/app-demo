e25451e5f2ec45871f5c9f765ec54b60a557545f
pedro-schawirin <schawirin@gmail.com>
2024-01-05 15:23:11 -0300

    lets go277
    go

--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -5,8 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./tracer"); // must come before importing any instrumented module.
 const express_1 = __importDefault(require("express"));
+require("dotenv/config");
+const dd_trace_1 = __importDefault(require("dd-trace"));
+dd_trace_1.default.init({
+    env: process.env.DD_ENV,
+    service: process.env.DD_SERVICE,
+    version: process.env.DD_VERSION,
+    // Outras configurações, se necessário
+});
 const app = (0, express_1.default)();
 const port = 3000;
+console.log("DD_ENV:", process.env.DD_ENV);
+console.log("DD_SERVICE:", process.env.DD_SERVICE);
+console.log("DD_VERSION:", process.env.DD_VERSION);
 // Servir arquivos estáticos da pasta 'public'
 app.use(express_1.default.static('public'));
 // Rota para gerar log
--- typescript/src/tracer.ts
+++
@@ -1,6 +0,0 @@
-// tracer.ts
-import tracer from 'dd-trace';
-
-tracer.init(); // Inicializa o tracer do Datadog
-
-export default tracer;
